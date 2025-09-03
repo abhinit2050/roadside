cff9f7e4c9ca4df6f43e0cd2d6451a0277c60545
Abhinit Rai <abhinit1990@gmail.com>
2025-09-03 07:47:46 +0530

    hoisting and temporal dead zone

--- var_let_const/script.js
+++ var_let_const/script.js
@@ -4,7 +4,7 @@ function test(){
     var b = "JavaScript";
 
     if(true){
-        var a = "Hi";       //Illegal shadowing as a has already been declared with let
+      //  var a = "Hi";       //Illegal shadowing as a has already been declared with let
         let b = "Python"    //This is not illegal as b is declared with var
          console.log(a);
         console.log(b);
@@ -19,8 +19,22 @@ test();
 
 var x; //No error
 let y;  //No error
-const z; //Error: Missing initializer in const declaration
+//const z; //Error: Missing initializer in const declaration
 const w = 10; //No error
 
 //Hoisting
+ var count;
 
+console.log(count); //undefined - but hoisting ensures count is recognised with current value as undefined
+count = 1;  //with this initialization, count is now 1
+
+//hoisting for let and const
+
+console.log(num); //Error: Cannot access 'num' before initialization
+let num = 5;
+
+//console.log(value); //Error: Cannot access 'value' before initialization
+const value = 15;
+
+//unless const and let are initialized, they cannot be used. 
+// They are in a "temporal dead zone" from the start of the block until the initialization is processed.
