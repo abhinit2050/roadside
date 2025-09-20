6ed3002363879ee60892ead5d7521168e9e14847
Abhinit Rai <abhinit1990@gmail.com>
2025-09-20 07:53:33 +0530

    polyfills for map filter reduce

--- mapFilterReduce/polyFills.js
+++ mapFilterReduce/polyFills.js
@@ -10,10 +10,30 @@ Array.prototype.myMap= function(cb){
      return temp;
 }
 
+Array.prototype.myFilter = function(cb){
+    let temp = [];
+    for(let i=0;i<this.length;i++){
+        if(cb(this[i],i,this)){
+            temp.push(this[i])
+        }
+    }
+    return temp;
+}
+
+Array.prototype.myReduce = function(cb, initialValue){
+    let acc = initialValue;
+
+    for(let i=0;i<this.length;i++){
+        acc = acc ? cb(acc,this[i],i,acc):this[i]
+    }
+
+    return acc;
+}
+
 let arr = [2,5,6,8];
 
-let result = arr.myMap((ele, index)=>{
-    return ele*2+index
-})
+let result = arr.myReduce((acc,curr)=>{
+    return acc = acc+curr;
+},0)
 
 console.log(result);
