75d134deb59b703e24683e1210709a88e1987381
Abhinit Rai <abhinit1990@gmail.com>
2025-10-27 18:22:44 +0530

    event loop part2

--- eventLoop/script.js
+++ eventLoop/script.js
@@ -1,9 +1,54 @@
-console.log("Start");
+//Q1
+// console.log("Start");
 
-setTimeout(()=>{console.log("sTo")},0)
+// setTimeout(()=>{console.log("sTo")},0)
 
-Promise.resolve().then(()=>{console.log("Promise")});
+// Promise.resolve().then(()=>{console.log("Promise")});
 
-console.log("End");
+// console.log("End");
 
 
+//Q2
+// blockMainThread();
+
+// console.log("start");
+
+// function blockMainThread(){
+//     const start = Date.now();
+    
+//     while(Date.now() - start <3000){}
+//     console.log("running...")
+// }
+
+// console.log("end");
+
+//Q3
+// setTimeout(()=>{console.log("a")},1000)
+// setTimeout(()=>{console.log("b")},500)
+// setTimeout(()=>{console.log("c")},0)
+
+// function d(){console.log("d")}
+
+// d();
+
+
+//Q4
+ function a(){
+    for(var i=0;i<3;i++){
+        setTimeout(()=>{
+            console.log(i)
+        },i*1000)
+    }
+}
+
+a();
+
+function b(){
+    for(let i=0;i<3;i++){
+        setTimeout(()=>{
+            console.log(i)
+        },i*1000)
+    }
+}
+
+b();
