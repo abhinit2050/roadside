4345c4608616538dede35ccd81ea7abe7909cf26
Abhinit Rai <abhinit1990@gmail.com>
2025-10-28 21:57:30 +0530

    event loop last part

--- eventLoop/script.js
+++ eventLoop/script.js
@@ -33,22 +33,74 @@
 
 
 //Q4
- function a(){
-    for(var i=0;i<3;i++){
-        setTimeout(()=>{
-            console.log(i)
-        },i*1000)
-    }
-}
+//  function a(){
+//     for(var i=0;i<3;i++){
+//         setTimeout(()=>{
+//             console.log(i)
+//         },i*1000)
+//     }
+// }
 
-a();
+// a();
 
-function b(){
-    for(let i=0;i<3;i++){
-        setTimeout(()=>{
-            console.log(i)
-        },i*1000)
-    }
-}
+// function b(){
+//     for(let i=0;i<3;i++){
+//         setTimeout(()=>{
+//             console.log(i)
+//         },i*1000)
+//     }
+// }
 
-b();
+// b();
+
+
+// Ques 5 - What's the Output?
+
+// Promise.resolve()
+//   .then(function a() {
+//     Promise.resolve().then(
+//         //with setTimeout d runs later because setTimeout goes to macrotask queue
+//     //   setTimeout(function d() {
+//     //     console.log("d Runs");
+//     //   }, 0)
+
+//         //without setTimeout d runs first
+//         function d() {
+//             console.log("d Runs");
+//           }
+//     );
+//     Promise.resolve().then(function e() {
+//       console.log("e Runs");
+//     });
+//     throw new Error("Error Occured!");  //this will trigger the catch block
+//     Promise.resolve().then(function f() {
+//       console.log("f Runs");
+//     });
+//   })
+//   .catch(function b() {
+//     console.log("b Runs");
+//   })
+//   .then(function c() {
+//     console.log("c Runs");
+//   });
+
+
+  //Q6
+  function pause(millis){
+    return new Promise(function p(resolve){
+        setTimeout(function s(){
+            resolve("resolved");
+        },millis)
+    })
+  }
+
+const start = Date.now();
+
+console.log('Start!');
+
+  pause(1000).then((res)=>{
+    const end = Date.now();
+    const secs = (end - start)/1000
+    console.log(res,":",secs)
+  }
+)
